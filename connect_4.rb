ece46221b69f6f5665e6fe01c0cf08e40ed05f46
malcolmbly <malcolmhbailey@gmail.com>
2022-12-23 14:09:03 -0700

    Added horizontal and vertical #game_over? checks, and fixed the testing methods for them

--- connect_4.rb
+++ connect_4.rb
@@ -1,23 +1,25 @@
 
 class ConnectFour
-  attr_reader :board, :turn
+  attr_accessor :board, :turn
   # 7 wide 6 high
+  # (0, 0) is top left of board, (5, 6) is bottom right
   # constructor takes board and player turn as optional inputs
   def initialize(board = [[0, 0, 0, 0, 0, 0, 0],
                           [0, 0, 0, 0, 0, 0, 0],
                           [0, 0, 0, 0, 0, 0, 0],
                           [0, 0, 0, 0, 0, 0, 0],
                           [0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0],
-                          turn = 1])
+                          [0, 0, 0, 0, 0, 0, 0]],
+                turn = 1)
     @board = board
     @turn = turn
+    @full_board = false
   end
 
   def start_game
     puts 'Welcome to connect four! Press any button to start'
     nil until gets
-    play_game
+  play_game
   end
 
   def play_game
@@ -31,7 +33,7 @@ class ConnectFour
   end
 
   def prompt_player_move
-    puts "Player #{@turn}, type in the column you'd live to place a piece in!"
+    puts "Player #{@turn}, type in the column you'd live to place a piece in! (between 1 and 7)"
     move = gets.chomp
     until valid_move?(move)
       puts 'Invalid move, enter the number of a column that has a free space (from 1 to 7).'
@@ -46,18 +48,69 @@ class ConnectFour
 
   def column_free?(move)
     # move is an integer representing a column of the board between 1 and 7.
-    @board.each do |row|
-      return true if row[move].zero?
-    end
-    false
+    # we decrement to go from common numbering to ruby indexing
+    move -= 1
+    @board[0][move].zero? ? true : false
   end
 
   def place_piece(move)
+    # decrement move by one to adjust from normal ways to refer
+    # to column to the indexing system
+    move -= 1
     @board.to_enum.with_index.reverse_each do |(row, index)|
       if row[move].zero?
         @board[index][move] = @turn
         break
       end
     end
+  end
+
+  def game_over?
+    # check that there's 4 in a row in any direction
+    # then that the board is full
+    # UNFINISHED
+    
+    @board.each.with_index do |row, row_index|
+      player_token_horizontal = 0
+      player_token_vertical = 0
+      consecutive_occurences_horizontal = 0
+      consecutive_occurences_vertical = 0
+
+      row.each.with_index do |slot, col_index|
+        if row_index.zero?
+          next_row_index = 1
+          player_token_vertical = slot
+          consecutive_occurences_vertical = 1
+          # vertical check for each column (just from top down)
+          until next_row_index > 5
+            next_vertical_slot = @board[next_row_index][col_index]
+            if next_vertical_slot == player_token_vertical
+              consecutive_occurences_vertical += 1
+            else
+              player_token_vertical = next_vertical_slot
+              consecutive_occurences_vertical = 1
+            end
+            return true if consecutive_occurences_vertical == 4
+
+            next_row_index += 1
+          end
+
+        end
+
+        # horizontal transversal
+        if slot == player_token_horizontal
+          consecutive_occurences_horizontal += 1
+        else
+          player_token_horizontal = slot
+          consecutive_occurences_horizontal = 1
+        end
+        return true if consecutive_occurences_horizontal == 4
+
+        # diagonal transversal
+        # the first 4 across and 3 down can be possible wins down right
+        # the last 4 across and first 3 downcan be possible wins down left
+      end
+    end
+    false
   end
 end
--- rspec/connect_4_spec.rb
+++ rspec/connect_4_spec.rb
@@ -30,6 +30,16 @@ describe ConnectFour do
         expect(subject.prompt_player_move).to eql('4')
       end
     end
+
+    context 'attempts to place a piece on a full column' do
+      subject(:game_start) { described_class.new }
+      it 'warns player of full column and asks for a new input' do
+        6.times { game_start.place_piece(1) }
+        allow(game_start).to receive(:gets).and_return('2', '3')
+        expect(game_start).to receive(:valid_move?).and_return(false, true)
+        game_start.prompt_player_move
+      end
+    end
   end
 
   describe '#place_piece' do
@@ -38,77 +48,66 @@ describe ConnectFour do
       it 'puts piece on bottom of an empty column' do
         game_start.place_piece(3)
         game_board = game_start.instance_variable_get(:@board)
-        expect(game_board[2][0]).to eql(1)
+        expect(game_board[5][2]).to eql(1)
       end
 
       it 'puts piece on top of a filled column spot' do
         game_start.turn = 2
         2.times { game_start.place_piece(3) }
         game_board = game_start.instance_variable_get(:@board)
-        expect(game_board[2][1]).to eql(2)
-      end
-    end
-
-    context 'When user provides invalid input' do
-      it 'warns player of invalid input, then asks for input again' do
-        allow(game_start).to receive(:gets).and_return('a', ' ')
-        expect(game_start).to receive(:puts).with('Error, invalid column choice!').twice
-        game_start.player_input(1)
-      end
-    end
-
-    context 'When user provides valid input on full column' do
-      it 'warns player of full column and asks for a new input' do
-        6.times { game_start.place_piece(1) }
-        allow(game_start).to receive(:gets).and_return('2')
-        expect(game_start).to receive(:puts).with('That column is full! Choose another.').once
-        game_start.place_piece(1)
+        expect(game_board[4][2]).to eql(2)
       end
     end
   end
 
   describe '#game_over?' do
-
     end_game_board = [[2, 2, 1, 2, 2, 2, 0],
-                      [1, 2, 1, 2, 2, 1, 2],
+                      [1, 2, 1, 2, 2, 2, 1],
                       [2, 2, 1, 1, 1, 2, 1],
-                      [1, 1, 2, 1, 1, 2, 1],
-                      [1, 1, 2, 2, 2, 1, 1],
-                      [1, 1, 1, 2, 1, 1, 2],
-                    ]
+                      [1, 1, 2, 1, 2, 1, 1],
+                      [1, 1, 2, 2, 2, 1, 2],
+                      [1, 1, 1, 2, 1, 1, 2]]
 
     tie_game_board = [[2, 2, 1, 2, 2, 2, 0],
                       [1, 2, 1, 2, 2, 2, 2],
                       [2, 2, 1, 1, 1, 2, 1],
                       [1, 1, 2, 1, 1, 2, 1],
                       [1, 1, 2, 2, 2, 1, 1],
-                      [1, 1, 1, 2, 1, 1, 2],
-                   ]                
-    
-
-    it 'returns true when player 1 wins' do
-      subject(:game_end) { described_class.new(end_game_board) }
-      game_end.player_move(7)
-      expect{ game_end.game_over? }.to eql(true)
+                      [1, 1, 1, 2, 1, 1, 2]]
+    context 'when player 2 wins horizontally' do
+      subject(:game_end) { described_class.new(end_game_board, 2) }
+      it 'returns true' do
+        game_end.place_piece(7)
+        expect(game_end.game_over?).to eql(true)
+      end
     end
 
-    it 'returns true when player 2 wins' do
-      subject(:game_end) { described_class.new(end_game_board, turn = 2) } 
-      game_end.player_move(7)
-      expect{ game_end.game_over? }.to eql(true)
-
+    context ' when player 1 wins vertically' do
+      subject(:game_end) { described_class.new(end_game_board, 1) }
+      it 'returns true' do
+        game_end.place_piece(7)
+        expect(game_end.game_over?).to eql(true)
+      end
     end
 
-    it 'returns true when the board is full' do
-      subject(:game_end) { described_class.new(tie_game_board, turn = 1) } 
-      game_end.player_move(7)
-      expect{ game_end.game_over? }.to eql(true)
+    context ' when player 1 wins diagonally' do
+      xit 'returns true' do
+      end
     end
 
-    it 'returns false when the board is empty' do
-      subject(:game_end) { described_class.new } 
-      expect{ game_end.game_over? }.to eql(false)
+    context 'when board is full without winners' do
+      subject(:game_end) { described_class.new(tie_game_board, 1) }
+      xit 'returns true' do
+        game_end.place_piece(7)
+        expect(game_end.game_over?).to eql(true)
+      end
+    end
+
+    context 'when board is empty' do
+      subject(:game_end) { described_class.new }
+      xit 'returns false' do
+        expect(game_end.game_over?).to eql(false)
+      end
     end
   end
-
 end
